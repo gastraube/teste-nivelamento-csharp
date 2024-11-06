f7bfeffa02814ef1a6daebfef7a97adae76cc02e
Gabriel Straube <ga.straube@gmail.com>
2024-11-06 12:22:54 -0300

    Add project files.

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    public class ContaBancaria
+    {
+        public int Numero { get; }
+        private string _nomeTitular;
+        public string NomeTitular
+        {
+            get => _nomeTitular;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _nomeTitular = value;
+                }
+            }
+        }
+
+        public double Saldo { get; private set; }
+
+        private const double TaxaSaque = 3.50;
+
+        public ContaBancaria(int numero, string nomeTitular, double depositoInicial = 0)
+        {
+            Numero = numero;
+            NomeTitular = nomeTitular;
+            Saldo = depositoInicial;
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+            else
+            {
+                throw new ArgumentException("O valor do depósito deve ser positivo.");
+            }
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo -= (valor + TaxaSaque);
+            }
+            else
+            {
+                throw new ArgumentException("O valor do saque deve ser positivo.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {NomeTitular}, Saldo: $ {Saldo}";
+        }
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+public class Program
+{
+    public const string API_URL = "https://jsonmock.hackerrank.com/api/football_matches";
+    public static void Main()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddTransient<HttpClient>();
+                services.AddTransient<IGames, Games>();
+
+            })
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<Games>(host.Services);
+        svc.Run().GetAwaiter().GetResult();
+    }
+
+    public interface IGames
+    {
+        Task Run();
+        string BuildUrl(int? year = null, string team1 = null, string team2 = null, int? page = null);
+        Task<int> GetTotalScoredGoalsAsync(string team, int year);
+    }
+
+    public class Games : IGames
+    {
+        private HttpClient _httpClient;
+        public Games(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task Run()
+        {
+            string teamName = "Paris Saint-Germain";
+            int year = 2013;
+            int totalGoals = await GetTotalScoredGoalsAsync(teamName, year);
+
+            Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+            teamName = "Chelsea";
+            year = 2014;
+            totalGoals = await GetTotalScoredGoalsAsync(teamName, year);
+
+            Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+            // Output expected:
+            // Team Paris Saint - Germain scored 109 goals in 2013
+            // Team Chelsea scored 92 goals in 2014
+        }
+
+        public async Task<int> GetTotalScoredGoalsAsync(string team, int year)
+        {
+            int totalGoals = 0;
+            int page = 1;
+
+            // Consultar gols como `team1`
+            while (true)
+            {
+                var urlTeam1 = BuildUrl(year, team, null, page);
+                var result = await FetchGoalsFromPage(urlTeam1, "team1goals");
+
+                if (result.Item2 == 0) break; // Sem mais páginas
+                totalGoals += result.Item2;
+                page++;
+            }
+
+            return totalGoals;
+        }
+
+        private async Task<(int, int)> FetchGoalsFromPage(string url, string goalField)
+        {
+            var response = await _httpClient.GetStringAsync(url);
+            var jsonDoc = JsonDocument.Parse(response);
+
+            // Verifica o total de páginas
+            int totalPages = jsonDoc.RootElement.GetProperty("total_pages").GetInt32();
+            int goalsInPage = 0;
+
+            foreach (var match in jsonDoc.RootElement.GetProperty("data").EnumerateArray())
+            {
+                goalsInPage += Convert.ToInt32(match.GetProperty(goalField).GetString());
+            }
+
+            return (totalPages, goalsInPage);
+        }
+
+        public string BuildUrl(int? year = null, string team1 = null, string team2 = null, int? page = null)
+        {
+            var baseUrl = API_URL;
+            var queryString = new StringBuilder();
+
+            if (year.HasValue)
+                queryString.Append($"year={year.Value}&");
+
+            if (!string.IsNullOrEmpty(team1))
+                queryString.Append($"team1={Uri.EscapeDataString(team1)}&");
+
+            if (!string.IsNullOrEmpty(team2))
+                queryString.Append($"team2={Uri.EscapeDataString(team2)}&");
+
+            if (page.HasValue)
+                queryString.Append($"page={page.Value}&");
+
+            if (queryString.Length > 0)
+                queryString.Length--;
+
+            return queryString.Length > 0 ? $"{baseUrl}?{queryString}" : baseUrl;
+        }
+
+    }
+}
