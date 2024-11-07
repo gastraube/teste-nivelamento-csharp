4bf238e540bde3ea374d9c61d0a1a2469aadb68f
Gabriel Straube <ga.straube@gmail.com>
2024-11-07 17:14:04 -0300

    Testes unitários

---
+++ Questao5/Tests/MovimentacaoHandlerTests.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using Questao5.Application.Commands.Requests;
+using Questao5.Application.Exceptions;
+using Questao5.Application.Handlers;
+using Questao5.Infrastructure.Database.Repositories;
+using Questao5.Infrastructure.Sqlite;
+using Xunit;
+
+namespace Questao5.Tests
+{
+    public class MovimentacaoHandlerTests
+    {
+        private Mock<IMovimentacaoRepository>? movimentacaoRepository;
+        private Mock<IContaRepository>? contarepoository;
+        private Mock<IIdempotenciaRepository>? idempotenciaRepository;
+
+        public void InitMovimentacaoHandler()
+        {
+            movimentacaoRepository = new Mock<IMovimentacaoRepository>();
+            contarepoository = new Mock<IContaRepository>();
+            idempotenciaRepository = new Mock<IIdempotenciaRepository>();
+        }
+
+        [Fact]
+        public async Task MovimentacaoHandler_InvalidAccount()
+        {
+            InitMovimentacaoHandler();
+
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
+            var saldoHandler = new MovimentacaoHandler(movimentacaoRepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new MovimentacaoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<INVALID_ACCOUNT>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task MovimentacaoHandler_InactiveAccount()
+        {
+            InitMovimentacaoHandler();
+
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
+            var saldoHandler = new MovimentacaoHandler(movimentacaoRepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new MovimentacaoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<INACTIVE_ACCOUNT>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task MovimentacaoHandler_IdempotencyAlreadyExists()
+        {
+            InitMovimentacaoHandler();
+
+            idempotenciaRepository.Setup(s => s.ChekIdempotencia(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
+            var saldoHandler = new MovimentacaoHandler(movimentacaoRepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new MovimentacaoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<Exception>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task MovimentacaoHandler_NegativeValue()
+        {
+            InitMovimentacaoHandler();
+
+            idempotenciaRepository.Setup(s => s.ChekIdempotencia(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+
+            var saldoHandler = new MovimentacaoHandler(movimentacaoRepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new MovimentacaoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid(), Valor = -1 };
+
+            await Assert.ThrowsAsync<INVALID_VALUE>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task MovimentacaoHandler_InvalidType()
+        {
+            InitMovimentacaoHandler();
+
+            idempotenciaRepository.Setup(s => s.ChekIdempotencia(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+
+            var saldoHandler = new MovimentacaoHandler(movimentacaoRepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new MovimentacaoRequest() { ChaveIdempotencia = Guid.NewGuid(),
+                IdConta = Guid.NewGuid(), Valor = 1, TipoMovimentacao = 'Z' };
+
+            await Assert.ThrowsAsync<INVALID_TYPE>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+    }
+}
---
+++ Questao5/Tests/SaldoHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using MediatR;
+using Moq;
+using Questao5.Application.Commands.Requests;
+using Questao5.Application.Exceptions;
+using Questao5.Application.Handlers;
+using Questao5.Infrastructure.Database.Repositories;
+using Xunit;
+
+namespace Questao5.Tests
+{
+    public class SaldoHandlerTests
+    {
+        private Mock<ISaldoRepository>? saldorepository;
+        private Mock<IContaRepository>? contarepoository;
+        private Mock<IIdempotenciaRepository>? idempotenciaRepository;
+
+        public void InitSaldoHandlerTests()
+        {
+            saldorepository = new Mock<ISaldoRepository>();
+            contarepoository = new Mock<IContaRepository>();
+            idempotenciaRepository = new Mock<IIdempotenciaRepository>();
+        }
+
+        [Fact]
+        public async Task SaldoHandler_InvalidAccount()
+        {
+            InitSaldoHandlerTests();
+
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
+            var saldoHandler = new SaldoHandler(saldorepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new GetSaldoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<INVALID_ACCOUNT>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task SaldoHandler_InactiveAccount()
+        {
+            InitSaldoHandlerTests();
+
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
+            var saldoHandler = new SaldoHandler(saldorepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new GetSaldoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<INACTIVE_ACCOUNT>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task SaldoHandler_IdempotencyAlreadyExists()
+        {
+            InitSaldoHandlerTests();
+
+            idempotenciaRepository.Setup(s => s.ChekIdempotencia(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckValidAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+            contarepoository.Setup(s => s.CheckActiveAccount(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+
+            var saldoHandler = new SaldoHandler(saldorepository.Object, contarepoository.Object, idempotenciaRepository.Object);
+            var command = new GetSaldoRequest() { ChaveIdempotencia = Guid.NewGuid(), IdConta = Guid.NewGuid() };
+
+            await Assert.ThrowsAsync<Exception>(() => saldoHandler.Handle(command, CancellationToken.None));
+        }
+    }
+}
